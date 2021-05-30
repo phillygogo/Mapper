0372a9a2bd05b072760adaa77a9fd2a03569ac60
phillygogo <phill-rooney@hotmail.com>
2021-05-30 13:44:46 +1200

    Initial commit

---
+++ src/js/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <Header />
+  <Main :books="$attrs.data.data.docs" />
+  <Footer />
+</div>
+</template>
+
+<script>
+	import Header from './components/Header.vue';
+	import Main from './components/Main.vue';
+	import Footer from './components/Footer.vue';
+	export default {
+		name: "App",
+		components: {
+			Header,
+			Main,
+      Footer,
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/js/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="container pt-6 pb-6 content">
+    <router-view name="/Home" :books="$attrs.books">
+      <slot-scope></slot-scope>
+    </router-view>
+  </main>
+</template>
+
+<script>
+	export default {
+		name: "Main"
+	}
+</script>
+
+<style scoped>
+
+</style>
