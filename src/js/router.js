79c5e1ba72821387d7c57bfd4346febadce2132e
phillygogo <phill-rooney@hotmail.com>
2021-05-30 13:55:07 +1200

    init project

--- src/js/router.js
+++ src/js/router.js
@@ -1,23 +1,23 @@
 // Routes are currently hardcoded - when wanting to add a new route create the new view in the views directory and then add it to the menu
 // If using this project for component only use please ignore routing
 
-import Router from "vue-router";
-import Vue from 'vue';
-import Home from "./views/Home.vue";
+// import Router from "vue-router";
+// import Vue from 'vue';
+// import Home from "./views/Home.vue";
 
-Vue.use(Router);
+// Vue.use(Router);
 
-const routes = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home,
-	},
-];
+// const routes = [
+// 	{
+// 		path: "/",
+// 		name: "Home",
+// 		component: Home,
+// 	},
+// ];
 
-const router = new Router({
-	routes,
-	mode: 'hash',
-});
+// const router = new Router({
+// 	routes,
+// 	mode: 'hash',
+// });
 
-export default router;
+// export default router;
